69fe6d52103282e3e648aee89c697840784a8af3
Eduardo Morales <spiderbbc@gmail.com>
2020-02-19 15:32:24 -0300

    adding params to newsapi.org

--- config/params.php
+++ config/params.php
@@ -11,6 +11,20 @@ return [
 	'adminEmail'  => 'eduardo@montana-studio.com',
 	'senderEmail' => 'eduardo@montana-studio.com',
 	'senderName'  => 'monitor-beta',
+	'newsApi'     => [
+		'time_hours_sleep' => 8,
+		'apiKey' => 'ca5709f738c146a0b5f86fad213c9316',
+		'targets' => [
+			'Forbes' => 'forbes.com',
+			'Techcrunch' => 'techcrunch.com',
+			'New York Times' => 'nytimes.com',
+			'Houston Chronicle' => 'chron.com',
+			'The Economist' => 'economist.com',
+			'Miami Herald' => 'miamiherald.com',
+			'Los Angeles Times' => 'latimes.com',
+			'The Wall Street Journal' => 'wsj.com'
+		],
+	],
 	'facebook'    => [ 
 		'time_min_sleep'  => 5,  
 		'business_id' => '169441517247',
