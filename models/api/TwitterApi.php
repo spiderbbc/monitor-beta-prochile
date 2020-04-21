51e5af82fe4593dcc2bc8964d8d88c1e53c7cb83
Eduardo Morales <spiderbbc@gmail.com>
2020-04-21 16:08:25 -0400

    change the way to took url form tweets

--- models/api/TwitterApi.php
+++ models/api/TwitterApi.php
@@ -391,11 +391,13 @@ class TwitterApi extends Model {
 						// source
 						$tweets[$product][$index]['source'] = $source;
 						// id tweets
+						$idTweet = $object[$o]['statuses'][$s]['id'];
 						$tweets[$product][$index]['id'] = $object[$o]['statuses'][$s]['id'];
 						// get user info
-						$tweets[$product][$index]['user'] = $this->_getUserData($object[$o]['statuses'][$s]);
+						$user = $this->_getUserData($object[$o]['statuses'][$s]);
+						$tweets[$product][$index]['user'] = $user;
 						// get entities url
-						$tweets[$product][$index]['url'] = $this->_getEntities($object[$o]['statuses'][$s]);
+						$tweets[$product][$index]['url'] = $this->_getUrlTweet($idTweet,$user['author_username']);
 
 
 						if(array_key_exists('place', $object[$o])){
@@ -444,30 +446,13 @@ class TwitterApi extends Model {
 	}
 
 	/**
-	 * [_getEntities get data from entities]
-	 * @param  [type] $tweet [tweet obejct]
+	 * [_getUrlTweet compose url  from url]
+	 * @param  [int] $idTweet [tweet id]
+	 * @param  [string] $user [user  screen_name]
 	 * @return [type]        [array]
 	 */
-	private function _getEntities($tweet){
-		$entities = [];
-		/*// get hashtags
-		if(isset($tweet['entities']['hashtags'])){
-			foreach($tweet['entities']['hashtags'] as $property => $value){
-				$entities['hashtags'][$property] = $value['text'];
-			}
-		}
-		// get user_mentions
-		if(isset($tweet['entities']['user_mentions'])){
-			foreach($tweet['entities']['user_mentions'] as $property => $value){
-				$entities['user_mentions'][$property][] = $value;
-			}
-		}*/
-		// get entities url
-		if(isset($tweet['entities']['urls'][0])){
-			$entities['url'] = $tweet['entities']['urls'][0]['url'];
-		}else{
-			$entities['url'] = '-';
-		}
+	private function _getUrlTweet($idTweet,$screen_name){
+		$entities['url'] = "https://twitter.com/${screen_name}/status/${idTweet}";
 		return $entities;
 	}
 
