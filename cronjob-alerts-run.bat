8a0a36d7b84c7b98209d2c2ae0cf33ee5d64559d
EduardoXavierMorales <xavierm@outlook.com>
2021-01-29 11:32:22 -0300

    delete .bat files

--- cronjob-alerts-run.bat
+++
@@ -1,5 +0,0 @@
-@Echo off
-title My Cron Job
-:: See title at the top
-php C:\wamp64\www\monitor-beta\yii daemon/alerts-run
-EXIT
--- cronjob-data-search.bat
+++
@@ -1,5 +0,0 @@
-@Echo off
-title My Cron Job
-:: See title at the top
-php C:\wamp64\www\monitor-beta\yii daemon/data-search
-EXIT
